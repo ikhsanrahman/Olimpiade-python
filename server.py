016ee58fc1d06c353c06bf59fb8a7c2d78055c3c
vickydasta <vicky.vernando@student.unri.ac.id>
2018-10-11 15:27:27 +0000

    add features

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ from flask import (
     redirect, 
     url_for, 
     flash, 
-    g)
+    g,
+    send_from_directory)
 
 from flask_login import (
     LoginManager, 
@@ -14,13 +15,15 @@ from flask_login import (
     current_user, 
     login_required)
 
+import csv
+
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 app = Flask("admin")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///prf.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///prf1.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "managaassQW"
 
@@ -53,6 +56,7 @@ class User(db.Model):
     kategori = db.Column('kategori', db.Text)
     kab_kota = db.Column('kab_kota', db.Text)
     provinsi = db.Column('provinsi', db.Text)
+    #skor_id = db.relationship("Skor", backref="user", lazy=True)
 
 
     def __init__(self, nama, email, alamat, sekolah,
@@ -136,27 +140,70 @@ class User(db.Model):
 class Soal(db.Model):
     __tablename__ = "soal"
     id = db.Column("id_soal", db.Integer, primary_key=True)
-    kategori = db.Column("kategori", db.Integer)
-    teks_soal = db.Column("teks_soal", db.Text)
+    kategori = db.Column(db.Text)
+    teks_soal = db.Column(db.Text)
+    opsi_a = db.Column(db.Text)
+    opsi_b = db.Column(db.Text)
+    opsi_c = db.Column(db.Text)
+    opsi_d = db.Column(db.Text)
+    posted_date = db.Column(db.DateTime)
 
-    def __init__(self, kategori, teks_soal):
+    def __init__(self, kategori, teks_soal, opsi_a, opsi_b, opsi_c, opsi_d):
         self.kategori = kategori
         self.teks_soal = teks_soal
+        self.opsi_a = opsi_a
+        self.opsi_b = opsi_b
+        self.opsi_c = opsi_c
+        self.opsi_d = opsi_d
+        self.posted_date = datetime.utcnow()
 
     def __repr__(self):
-        return "<Soal %r>"% (self.nama)
+        return "<Soal %r>"% (self.id)
+
 
 class Pengumuman(db.Model):
     __tablename__ = "pengumuman"
-    id = db.Column("id_soal", db.Integer, primary_key=True)
-    konten = db.Column("teks_soal", db.Text)
+    id = db.Column("id", db.Integer, primary_key=True)
+    kategori = db.Column(db.Text)
+    judul = db.Column(db.Text)
+    konten = db.Column(db.Text)
+    posted_date = db.Column(db.DateTime)
 
-    def __init__(self, kategori, teks_soal):
+    def __init__(self, kategori, judul, konten):
         self.kategori = kategori
-        self.teks_soal = teks_soal
+        self.judul = judul
+        self.konten = konten
+        self.posted_date = datetime.utcnow()
+
 
     def __repr__(self):
-        return "<Soal %r>"% (self.nama)
+        return "<Pengumuman %r>"% (self.id)
+
+
+class Lomba(db.Model):
+    __tablename__ = "lomba"
+    id = db.Column(db.Integer, primary_key=True)
+    mulai = db.Column(db.Boolean, default=False)
+    start = db.Column(db.DateTime)
+    durasi = db.Column(db.Integer)
+
+
+    def __init__(self, start, durasi):
+        self.mulai = False
+        self.start = start
+        self.durasi = durasi
+
+
+    def mulai_lomba(self):
+        self.start = True
+
+
+class Skor(db.Model):
+    __tablename__ = "skor"
+    id = db.Column(db.Integer, primary_key=True)
+    skor = db.Column(db.Integer, default=0)
+    id_user = db.Column(db.Integer, db.ForeignKey("user.id"))
+
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -256,16 +303,153 @@ def manage_user():
 @login_required
 def add_soal():
     if current_user.role == "admin":
-        return render_template("soal.html")
+        soal = Soal.query.all()
+        return render_template("soal.html", soal=soal)
     # else return forbidden message!
+
+
+@app.route("/tambahsoal", methods=["GET", "POST"])
+@login_required
+def tambah_soal():
+    if current_user.role == "admin":
+        if request.method == "POST":
+            kategori = request.form.get("kategori")
+            teks_soal = request.form["teks_soal"]
+            opsi_a = request.form["pilihan_a"]
+            opsi_b = request.form["pilihan_b"]
+            opsi_c = request.form["pilihan_c"]
+            opsi_d = request.form["pilihan_d"]
+
+            soal = Soal(kategori=kategori, teks_soal=teks_soal, opsi_a=opsi_a, opsi_b=opsi_b, opsi_c=opsi_c, opsi_d=opsi_d)
+            db.session.add(soal)
+            db.session.commit()
+            
+            return redirect(url_for("add_soal"))
+
+        return render_template("tambah_soal.html")
+
+    return redirect(url_for("index_page"))
+
+@app.route("/editsoal/<id>", methods=["GET", "POST"])
+def edit_soal(id):
+    if current_user.role == "admin":
+        if id:
+            if request.method == "POST":
+                kategori = request.form.get("kategori")
+                teks_soal = request.form["teks_soal"]
+                opsi_a = request.form["pilihan_a"]
+                opsi_b = request.form["pilihan_b"]
+                opsi_c = request.form["pilihan_c"]
+                opsi_d = request.form["pilihan_d"]
+
+                soal = Soal.query.filter_by(id=id).first()
+
+                soal.kategori = kategori
+                soal.teks_soal = teks_soal
+                soal.opsi_a = opsi_a
+                soal.opsi_b = opsi_b
+                soal.opsi_c = opsi_c
+                soal.opsi_d = opsi_d
+
+                db.session.commit()
+                
+                return redirect(url_for("add_soal"))
+
+            soal = Soal.query.filter_by(id=id).first()
+
+            if soal:
+                return render_template("editsoal.html", soal=soal)
+
+            return redirect(url_for("add_soal"))
+        
+        return redirect(url_for("add_soal"))
+
+
+@app.route("/deletesoal/<id>", methods=["POST"])
+def delete_soal(id):
+    if current_user.role == "admin":
+        if id:
+            if request.method == "POST":
+                soal = Soal.query.filter_by(id=id).first()
+                if soal:
+                    db.session.delete(soal)
+                    db.session.commit()
+                    return redirect(url_for("add_soal"))
+                return redirect(url_for("add_soal"))
+
+
 
 
 @app.route("/pengumuman")
 @login_required
 def pengumuman():
     if current_user.role == "admin":
-        return render_template("pengumuman.html")
-    # else return forbidden message!
+        pengumuman = Pengumuman.query.all()
+        return render_template("pengumuman.html", pengumuman=pengumuman)
+
+    return redirect(url_for("index_page"))
+
+
+
+@app.route("/tambahpengumuman", methods=["GET", "POST"])
+@login_required
+def tambah_pengumuman():
+    if current_user.role == "admin":
+        if request.method == "POST":
+            kategori = request.form.get("kategori")
+            judul = request.form["judul"]
+            konten = request.form["konten"]
+
+            pengumuman = Pengumuman(kategori=kategori, judul=judul, konten=konten)
+            db.session.add(pengumuman)
+            db.session.commit()
+
+            return redirect(url_for("pengumuman"))
+
+        return render_template("tambah_pengumuman.html")
+
+
+@app.route("/editpengumuman/<id>", methods=["GET", "POST"])
+def edit_pengumuman(id):
+    if current_user.role == "admin":
+        if id:
+            if request.method == "POST":
+                kategori = request.form.get("kategori")
+                judul = request.form["judul"]
+                konten = request.form["konten"]
+
+                pengumuman = Pengumuman.query.filter_by(id=id).first()
+
+                pengumuman.kategori = kategori
+                pengumuman.judul = judul
+                pengumuman.konten = konten
+
+                db.session.commit()
+                
+                return redirect(url_for("pengumuman"))
+
+            pengumuman = Pengumuman.query.filter_by(id=id).first()
+
+            if pengumuman:
+                return render_template("editpengumuman.html", pengumuman=pengumuman)
+
+            return redirect(url_for("pengumuman"))
+        
+        return redirect(url_for("pengumuman"))
+
+
+@app.route("/deletepengumuman/<id>", methods=["POST"])
+def delete_pengumuman(id):
+    if current_user.role == "admin":
+        if id:
+            if request.method == "POST":
+                pengumuman = Pengumuman.query.filter_by(id=id).first()
+                if pengumuman:
+                    db.session.delete(pengumuman)
+                    db.session.commit()
+                    return redirect(url_for("pengumuman"))
+
+                return redirect(url_for("pengumuman"))
 
 
 @app.route('/confirmed')
@@ -275,6 +459,7 @@ def user_confirmed():
         user = User.query.filter_by(role='user').filter_by(confirmed=True)
         return render_template("admin.html", confirmed_users=user)
 
+    return redirect(url_for("index_page"))
 
 @app.route("/confirm/<id>", methods=["POST"])
 @login_required
@@ -290,6 +475,8 @@ def confirm_user(id):
                 db.session.commit()
                 return redirect(url_for("manage_user"))
 
+    return redirect(url_for("index_page"))
+
 
 @app.route("/undoconfirm/<id>", methods=["POST"])
 @login_required
@@ -303,6 +490,46 @@ def undoconfirm_user(id):
                 db.session.commit()
                 return redirect(url_for("user_confirmed"))
 
+
+@app.route("/download/peserta")
+@login_required
+def download_peserta():
+
+    if current_user.role == "admin":
+        data_user = User.query.filter_by(confirmed=True)
+
+        with open("static/datapeserta.csv", "w+") as f:
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow(("nama", "email", "registered_date", "alamat", "sekolah", "jenis_kelamin", "kategori", "kab_kota", "provinsi"))
+
+            for user in data_user:
+                writer.writerow((user.nama, user.email, user.registered_date, user.alamat, 
+                                 user.sekolah, user.jenis_kelamin, user.kategori, user.kab_kota, user.provinsi))
+
+        return send_from_directory(directory="static", filename="datapeserta.csv")
+            
+
+@app.route("/download/pendaftar")
+@login_required
+def download_pendaftar():
+    if current_user.role == "admin":
+        data_user = User.query.filter_by(confirmed=False)
+
+        with open("static/datapendaftar.csv", "w+") as f:
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow(("nama", "email", "registered_date", "alamat", "sekolah", "jenis_kelamin", "kategori", "kab_kota", "provinsi"))
+
+            for user in data_user:
+                writer.writerow((user.nama, user.email, user.registered_date, user.alamat, user.sekolah, user.jenis_kelamin, user.kategori, user.kab_kota, user.provinsi))
+
+        return send_from_directory(directory="static", filename="datapendaftar.csv")
+
+
+@app.route("/download")
+@login_required
+def download():
+    if current_user.role == "admin":
+        return render_template("download.html")
 
 ################################
 # USER AREA
@@ -327,5 +554,6 @@ def user_info():
 @login_required
 def main_app():
     if current_user.role == "user":
-        return render_template("app.html", title="selamat datang di PRF Nasional!")
+        pengumuman = Pengumuman.query.all()
+        return render_template("app.html", title="selamat datang di PRF Nasional!", pengumuman=pengumuman)
     # return 403 error
